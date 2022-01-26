433d8ef118202c4402f38e8be8c5841adbea80ac
Luca Foppiano <Foppiano.Luca@nims.go.jp>
2022-01-26 16:40:39 +0900

    add script to feedback corrections and tests

---
+++ tests/test_feedback_corrections.py
@@ -0,0 +1,33 @@
+from bson import ObjectId
+
+from process.feedback_corrections import process
+
+
+def test_database(mongodb):
+    assert 'tabular' in mongodb.list_collection_names()
+    record = mongodb.tabular.find_one({'hash': '48ba234393'})
+    assert record is not None
+
+
+def test_process_excel_check_obsolete_links(mongodb):
+    process('resources/supercon_corrected.xlsx', mongodb)
+
+    tabular_collection = mongodb.get_collection("tabular")
+    records_all = tabular_collection.find({"hash": "48ba234393"})
+    assert records_all.count() == 6
+
+    records_manually_corrected = list(tabular_collection.find({"hash": "48ba234393", "status": "valid", "type":"manual"}))
+    assert len(records_manually_corrected) == 2
+
+    for record_manually_corrected in records_manually_corrected:
+        assert tabular_collection.find_one({"_id": record_manually_corrected['previous']})["status"] == "obsolete"
+
+    records_obsolete = tabular_collection.find({"hash": "48ba234393", "status": "obsolete"})
+    assert records_obsolete.count() == 2
+
+
+def test_process_excel_verify_training_data(mongodb):
+    process('resources/supercon_corrected.xlsx', mongodb)
+
+    training_data_collection = mongodb.get_collection("training-data")
+    assert training_data_collection.count() == 2
