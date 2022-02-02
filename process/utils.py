91826b6b4304a010feba1dd951942a1c3709d84b
Luca Foppiano <Foppiano.Luca@nims.go.jp>
2022-02-02 18:09:29 +0900

    fix serialisation of objectid

--- process/utils.py
+++ process/utils.py
@@ -1,9 +1,13 @@
 from datetime import datetime, date
 
+from bson import ObjectId
+
 
 def json_serial(obj):
     """JSON serializer for objects not serializable by default json code"""
 
     if isinstance(obj, (datetime, date)):
         return obj.isoformat()
+    if isinstance(obj, (ObjectId)):
+        return str(obj)
     raise TypeError("Type %s not serializable" % type(obj))
