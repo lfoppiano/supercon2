3455c838bf6d4f32d31a771f32abd3c8f9366b1f
Luca Foppiano <Foppiano.Luca@nims.go.jp>
2023-07-07 17:11:51 +0900

    update anomaly detection script

--- process/anomaly_detection.py
+++ process/anomaly_detection.py
@@ -1,17 +1,19 @@
 import argparse
+import json
 import os
 import re
 import sys
 from pathlib import Path
 
 import pymatgen.core as mg
+import requests
 
 from commons.correction_utils import write_raw_training_data
 from process.grobid_client_generic import GrobidClientGeneric
 from supercon_batch_mongo_extraction import connect_mongo
 
 
-def validate_formula(formula):
+def validate_formula_strict(formula):
     try:
         mg.Composition(formula, strict=False)
     except:
@@ -24,7 +26,38 @@ def validate_formula(formula):
     return True, ""
 
 
-def parse_tc(tc) -> float:
+def validate_formula_naive(formula):
+    formula_parser_url = "http://falcon.nims.go.jp/material/nlp/stable/convert/formula/composition"
+
+    data = {'input': formula}
+    response = requests.post(formula_parser_url, data=data)
+
+    if response.status_code != 200:
+        if response.status_code == 400:
+            return False, ""
+        else:
+            print("Cannot parse the formula")
+            return True, ""
+
+    else:
+        if response.text == "{}":
+            return False, ""
+
+        return True, ""
+        # comp = json.loads(response.text)['composition']
+        #
+        # result = ''
+        # for key, value in comp.items():
+        #     if value == '1':
+        #         result += key + ' '
+        #     else:
+        #         result += key + value + ' '
+        #
+        # line.insert(header.index("parsed_formula"), comp)
+        # line.insert(header.index("formatted_formula"), result)
+
+
+def parse_tc_regex(tc) -> float:
     search = temp_regex.search(tc)
     if search:
         value = search.groups()[0]
@@ -41,6 +74,58 @@ def parse_tc(tc) -> float:
             raise Exception("Value not in Kelvin. Might not be a Tc. ")
     else:
         raise Exception("No Tc found")
+
+
+grobid_quantities_url = "http://falcon.nims.go.jp/quantities/stable"
+# grobid_quantities_url = "https://lfoppiano-grobid-quantities.hf.space"
+quantity_text_url = grobid_quantities_url + "/service/processQuantityText"
+
+
+def parse_quantity(tc, is_not_tc=False):
+    data = str({'text': tc})
+    response = requests.post(quantity_text_url, files={"text": data})
+
+    if response.status_code == 200:
+        parsed_response = json.loads(response.text)
+        if 'measurements' in parsed_response:
+            quantities_value = []
+            for found_measurement in parsed_response['measurements']:
+                type = found_measurement['type']
+                if type == 'value':
+                    quantity_obj = found_measurement['quantity']
+                    quantity_value = get_value_as_float(quantity_obj)
+                    quantities_value.append(quantity_value)
+                elif type == 'interval':
+                    if 'quantityLeast' in found_measurement:
+                        quantity_obj = found_measurement['quantityLeast']
+                        quantities_value.append(get_value_as_float(quantity_obj))
+                    if 'quantityMost' in found_measurement:
+                        quantity_obj = found_measurement['quantityMost']
+                        quantities_value.append(get_value_as_float(quantity_obj))
+
+                elif type == 'listc':
+                    quantities_obj = found_measurement['quantities']
+                    for quantity_obj in quantities_obj:
+                        quantities_value.append(get_value_as_float(quantity_obj))
+
+            return quantities_value
+
+        else:
+            if not is_not_tc:
+                float_value = parse_tc_regex(tc)
+                return [float_value]
+
+
+def get_value_as_float(quantity_obj):
+    quantity_value = 0.0
+    if 'normalizedUnit' in quantity_obj:
+        normalized_quantity = quantity_obj['normalizedQuantity']
+        # normalized_unit = quantity_obj['normalizedUnit']['name']
+        quantity_value = float(normalized_quantity)
+    elif 'parsedValue' in quantity_obj:
+        parsed_quantity = quantity_obj['parsedValue']['parsed']
+        quantity_value = float(parsed_quantity)
+    return quantity_value
 
 
 if __name__ == '__main__':
@@ -78,15 +163,15 @@ if __name__ == '__main__':
 
     temp_regex = re.compile(r"^([0-9.]+) ?(m?K{1})$")
     # tabular_cursor = tabular_collection.find({"status": {"$not": {"$in": ["empty", "invalid", "removed", "obsolete"]}}})
-    tabular_cursor_count = tabular_collection.count_documents({"status": {"$in": ["new"]}})
-    tabular_cursor = tabular_collection.find({"status": {"$in": ["new"]}})
+    tabular_cursor_count = tabular_collection.count_documents({"status": {"$in": ["new", "invalid"]}})
+    tabular_cursor = tabular_collection.find({"status": {"$in": ["new", "invalid"]}})
     anomalies = []
     materials = {}
 
     float_value = -1.0
     for record in tabular_cursor:
         tc = record['criticalTemperature']
-        tc_float_value = 0.0
+        # tc_float_value = 0.0
         if tc:
             anomaly = {
                 "id": record['_id'],
@@ -94,19 +179,22 @@ if __name__ == '__main__':
                 "value": tc
             }
             try:
-                float_value = parse_tc(tc)
-
-                if float_value > 270:
-                    anomaly['description'] = "Tc too high"
-                    anomalies.append(anomaly)
+                float_values = parse_quantity(tc)
+                is_anomaly = False
+                for float_value in float_values:
+                    if float_value > 270:
+                        anomaly['description'] = "Tc too high"
+                        anomalies.append(anomaly)
+                        is_anomaly = True
+                    elif float_value < 0:
+                        anomaly['description'] = "Tc negative"
+                        anomalies.append(anomaly)
+                        is_anomaly = True
+                if is_anomaly:
                     continue
-                elif float_value < 0:
-                    anomaly['description'] = "Tc negative"
-                    anomalies.append(anomaly)
-                    continue
 
-            except:
-                anomaly['description'] = "Tc not parseable"
+            except Exception as e:
+                anomaly['description'] = "Tc not parseable, " + str(e)
                 anomalies.append(anomaly)
                 continue
 
@@ -114,11 +202,26 @@ if __name__ == '__main__':
             anomaly = {"id": record['_id'], "type": "tc", "value": tc, 'description': "Tc null or empty"}
             anomalies.append(anomaly)
 
+        applied_pressure = record['appliedPressure']
+        if applied_pressure:
+            anomaly = {
+                "id": record['_id'],
+                "type": "applied_pressure",
+                "value": applied_pressure
+            }
+
+            try:
+                parse_quantity(applied_pressure, is_not_tc=True)
+            except Exception as e:
+                anomaly['description'] = "Applied pressure not parseable, " + str(e)
+                anomalies.append(anomaly)
+                continue
+
         formula = record['formula'] if 'formula' in record else None
         variables = record['variables'] if 'variables' in record else None
 
         if formula and not variables:
-            valid, exception = validate_formula(formula)
+            valid, exception = validate_formula_naive(formula)
             if not valid:
                 anomalies.append(
                     {
@@ -165,35 +268,37 @@ if __name__ == '__main__':
     else:
         print("Writing on the database:", db_name, config['mongo']['server'])
         input_value = input("Continue [y/N]")
-        if input_value == "y":
-            document_collection = db.get_collection("document")
-            training_data_collection = db.get_collection("training_data")
-            for anomaly in anomalies:
-                if verbose:
-                    print(anomaly['type'], "-", anomaly['value'], "-", anomaly['description'])
+        pass
+        # if input_value == "y":
+        #     document_collection = db.get_collection("document")
+        #     training_data_collection = db.get_collection("training_data")
+        #     for anomaly in anomalies:
+        #         if verbose:
+        #             print(anomaly['type'], "-", anomaly['value'], "-", anomaly['description'])
+        #
+        #             old_doc = tabular_collection.find_one({'_id': anomaly['id'], "status": "new"})
+        #             if old_doc:
+        #                 new_status = 'invalid'
+        #                 new_type = 'automatic'
+        #                 error_type = "anomaly_detection"
+        #
+        #                 changes = {'status': new_status, 'type': new_type, 'error_type': error_type}
+        #
+        #                 tabular_collection.update_one({'_id': anomaly['id']}, {'$set': changes})
+        #                 training_data_id = write_raw_training_data(old_doc, anomaly['id'], document_collection,
+        #                                                            training_data_collection,
+        #                                                            action="anomaly")
+        #
+        #             else:
+        #                 print("Document", anomaly['id'], "not found, ignoring it.")
 
-                    old_doc = tabular_collection.find_one({'_id': anomaly['id'], "status": "new"})
-                    if old_doc:
-                        new_status = 'invalid'
-                        new_type = 'automatic'
-                        error_type = "anomaly_detection"
 
-                        changes = {'status': new_status, 'type': new_type, 'error_type': error_type}
-
-                        tabular_collection.update_one({'_id': anomaly['id']}, {'$set': changes})
-                        training_data_id = write_raw_training_data(old_doc, anomaly['id'], document_collection,
-                                                                   training_data_collection,
-                                                                   action="anomaly")
-
-                    else:
-                        print("Document", anomaly['id'], "not found, ignoring it.")
-
-
-        else:
-            print("Aborting")
+        # else:
+        #     print("Aborting")
 
     print("Total number of records", tabular_cursor_count)
     print("Anomalies in Tc:", len(list(filter(lambda x: x['type'] == "tc", anomalies))))
+    print("Anomalies in applied pressure:", len(list(filter(lambda x: x['type'] == "applied_pressure", anomalies))))
     print("Anomalies in Materials formula:", len(list(filter(lambda x: x['type'] == "formula", anomalies))))
     print("Anomalies in materials with multiple Tc: ", len(duplicated_materials),
           "with outliers", nb_outliers, "/", total_duplicated_tc)
